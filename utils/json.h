7f6549c68ea29845a5b3d3a00285be3b1db0657f
espinosa05 <francisco-fischer@gmx.de>
2024-11-19 21:04:38 +0100

    remove unused library

--- utils/json.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ESPI_JSON_H__
-#define __ESPI_JSON_H__
-
-JSON_Status json_load_from_path(struct json *json, const char *path);
-JSON_Status json_load_from_fd(struct json *json, int fd);
-
-JSON_Status json_cleanup(struct json *json);
-
-#endif /* __ESPI_JSON_H__ */
